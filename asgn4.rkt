af6dd6ea481ba3aca3b5a15b51ee706d66976ccc
munozAndrew <115670550+munozAndrew@users.noreply.github.com>
2024-10-19 21:01:26 -0700

    Update asgn4.rkt

--- asgn4.rkt
+++ asgn4.rkt
@@ -32,9 +32,19 @@
                  [env : Env]))
 (struct PrimOpC ([op : Symbol]))
 (struct Clause ([id : Symbol] [expr : ExprC]))
-(struct Binding ([name : Symbol] [val : Real]))
+(struct Binding ([name : Symbol] [val : Value]))
 (define-type Env (Listof Binding))
 (struct FunDefC ([name : Symbol] [args : (Listof Symbol)] [body : ExprC])#:transparent)
+
+
+
+(define-type Value(U NumV BoolV StringV ClosureV PrimOpV))
+
+(struct NumV ([n : Real])#:transparent)
+(struct BoolV ([b : Boolean])#:transparent)
+(struct StringV ([s : String])#:transparent)
+(struct ClosureV ([params : (Listof Symbol)] [body : ExprC] [env : Env])#:transparent)
+(struct PrimOpV ([op : Symbol])#:transparent)
 
 (define mt-env '())
 (define extend-env cons)
@@ -46,9 +56,11 @@
 ;;current env mapping ids to their vals - Env
 ;;list of func defs available for interp - fds
 ;;Output: the result of evaling the exp - Real
- (define (interp [expr : ExprC] [env : Env] [fds : (Listof FunDefC)]) : Real
+ (define (interp [expr : ExprC] [env : Env] [fds : (Listof FunDefC)]) : Value
     (match expr
-      [(NumC n) n]
+      [(NumC n) (NumV n)]
+      [(BoolC b) (BoolV b)]
+      [(StringC s) (StringV s)]
       [(IdC s) (lookup s env)]
       ;<idC-case>
       ;<appC-case>
@@ -61,7 +73,7 @@
 ;; symbol whose val needs to be retrieved - for
 ;; env is the current environmen that maps the id to its value - env
 ;;Output: the val pertaining to sym
-(define (lookup [for : Symbol] [env : Env]) : Real
+(define (lookup [for : Symbol] [env : Env]) : Value
   (match env
     ['() (error 'lookup "AAQZ: name not found: ~e" for)]
     [(cons (Binding name val) r)
